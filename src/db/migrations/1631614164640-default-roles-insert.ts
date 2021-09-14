e334309e802fac3436492ccec60f82aced1a1af2
Shpackk <victorija45@gmail.com>
2021-09-14 19:24:35 +0300

    test migration

---
+++ src/db/migrations/1631614164640-default-roles-insert.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class defaultRolesInsert1631614164640 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+
+    }
+    
+}
