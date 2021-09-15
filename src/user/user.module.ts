473531d8d36f261d3fb9979ba315c095956d2d0d
Shpackk <victorija45@gmail.com>
2021-09-15 20:11:53 +0300

    imports of banlist requests

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtStrategy } from 'src/auth/jwt.strategy';
+import { Banlist } from 'src/db/entity/banlist.entity';
+import { Requests } from 'src/db/entity/requests.entity';
 import { Roles } from 'src/db/entity/roles.entity';
 import { User } from 'src/db/entity/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([User, Roles])],
+  imports: [
+    TypeOrmModule.forFeature([User, Roles, Requests, Banlist]),
+    JwtModule.register({
+      secret: 'supersecret',
+      signOptions:{expiresIn: '15m'}
+    })
+  ],
   controllers: [UserController],
   providers: [UserService, JwtStrategy]
 })
