a3f2c18374ca564d723e07d76344971fa3ba056c
Shpackk <victorija45@gmail.com>
2021-09-16 19:18:41 +0300

    queries moved to postgresquery

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,19 +5,23 @@ import { JwtStrategy } from 'src/auth/jwt.strategy';
 import { Banlist } from 'src/db/entity/banlist.entity';
 import { Requests } from 'src/db/entity/requests.entity';
 import { Roles } from 'src/db/entity/roles.entity';
+import { Team } from 'src/db/entity/teams.entity';
 import { User } from 'src/db/entity/user.entity';
+import { requestsQueries } from 'src/postgrQuery/requests-table-queries';
+import { teamQueries } from 'src/postgrQuery/team-table-queries';
+import { userQueries } from 'src/postgrQuery/user-table-queries';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Roles, Requests, Banlist]),
+    TypeOrmModule.forFeature([User, Roles, Requests, Banlist, Team]),
     JwtModule.register({
       secret: 'supersecret',
       signOptions:{expiresIn: '15m'}
     })
   ],
   controllers: [UserController],
-  providers: [UserService, JwtStrategy]
+  providers: [UserService, JwtStrategy,userQueries,requestsQueries,teamQueries]
 })
 export class UserModule {}
