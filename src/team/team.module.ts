9034669244c47a6727aa9c05f698eabf0981230c
Shpackk <victorija45@gmail.com>
2021-09-16 19:17:16 +0300

    queries moved to postgrqueries

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtStrategy } from 'src/auth/jwt.strategy';
 import { Requests } from 'src/db/entity/requests.entity';
+import { Team } from 'src/db/entity/teams.entity';
 import { User } from 'src/db/entity/user.entity';
+import { requestsQueries } from 'src/postgrQuery/requests-table-queries';
+import { userQueries } from 'src/postgrQuery/user-table-queries';
 import { TeamController } from './team.controller';
 import { TeamService } from './team.service';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Requests, User])],
+  imports:[TypeOrmModule.forFeature([Requests, User,Team])],
   controllers: [TeamController],
-  providers: [TeamService, JwtStrategy]
+  providers: [TeamService, JwtStrategy, userQueries,requestsQueries]
 })
 export class TeamModule {}
