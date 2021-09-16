d155718bc93bb79f70d6a823b8046afcf4885d84
Shpackk <victorija45@gmail.com>
2021-09-16 19:16:23 +0300

    queries moved to postgrquery

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,21 +2,24 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Banlist } from 'src/db/entity/banlist.entity';
+import { Requests } from 'src/db/entity/requests.entity';
 import { Roles } from 'src/db/entity/roles.entity';
 import { User } from 'src/db/entity/user.entity';
+import { requestsQueries } from 'src/postgrQuery/requests-table-queries';
+import { userQueries } from 'src/postgrQuery/user-table-queries';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Roles, Banlist]),
+    TypeOrmModule.forFeature([User, Roles, Banlist, Requests]),
     JwtModule.register({
       secret: 'supersecret',
       signOptions:{expiresIn: '24h'}
     })
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy]
+  providers: [AuthService, JwtStrategy,userQueries,requestsQueries]
 })
 export class AuthModule {}
