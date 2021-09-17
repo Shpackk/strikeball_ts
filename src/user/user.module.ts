3f8f29a7add2fdc1658497eb8bc5dfa0442c165e
Shpackk <victorija45@gmail.com>
2021-09-17 19:47:31 +0300

    middleware to allow only admin hit endpoint

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtStrategy } from 'src/auth/jwt.strategy';
+import { JwtStrategy } from 'src/passport/jwt.strategy';
 import { Banlist } from 'src/db/entity/banlist.entity';
 import { Requests } from 'src/db/entity/requests.entity';
 import { Roles } from 'src/db/entity/roles.entity';
 import { Team } from 'src/db/entity/teams.entity';
 import { User } from 'src/db/entity/user.entity';
-import { requestsQueries } from 'src/postgrQuery/requests-table-queries';
-import { teamQueries } from 'src/postgrQuery/team-table-queries';
-import { userQueries } from 'src/postgrQuery/user-table-queries';
+import { requestsQueries } from 'src/repositoriers/requests-table';
+import { teamQueries } from 'src/repositoriers/team-table';
+import { userQueries } from 'src/repositoriers/user-table';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
