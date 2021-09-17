60fc830e9e22065c89f38e6a720d01ecb55b3ace
Shpackk <victorija45@gmail.com>
2021-09-17 19:52:58 +0300

    feature to upload files via multer

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,26 @@
-import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { diskStorage } from 'multer';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
 import { RegUserDto } from './dto/register-user.dto';
-import { JwtAuthGuard } from './jwt-auth.guard';
+import { JwtAuthGuard } from 'src/passport/jwt-auth.guard';
+import { fileNameGen, imageFileFilter } from 'src/services/fileUploadHandler';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
     
     @Post('/register')
-    async register(@Body() user: RegUserDto) {
-        return await this.authService.register(user)
+    @UseInterceptors(FileInterceptor('picture', {
+        storage: diskStorage({
+            destination: './uploads/',
+            filename: fileNameGen,
+        }),
+    fileFilter: imageFileFilter
+  }))
+    async register(@Body() user: RegUserDto, @UploadedFile() file: Express.Multer.File) {
+        return await this.authService.register(user,file)
     }
     
     @Post('/login')
