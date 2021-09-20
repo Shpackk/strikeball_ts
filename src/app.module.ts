3cd29a39f3bdb7cdff6be54d46c81067a8a75c64
Shpackk <victorija45@gmail.com>
2021-09-21 01:55:40 +0300

    mail module imported

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TeamModule } from './team/team.module';
+import { MailModule } from './mail/mail.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     UserModule,
     AuthModule,
-    TeamModule
+    TeamModule,
+    MailModule
   ],
   controllers: [AppController],
   providers: [AppService],
