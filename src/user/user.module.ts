1b557b717a86cad92b7d0288899781fe97bf7b9b
Shpackk <victorija45@gmail.com>
2021-09-21 00:45:00 +0300

    middleware to check roles

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, Req, RequestMethod } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtStrategy } from 'src/passport/jwt.strategy';
+import { JwtStrategy } from 'src/passport/jwtauth/jwt.strategy';
 import { Banlist } from 'src/db/entity/banlist.entity';
 import { Requests } from 'src/db/entity/requests.entity';
 import { Roles } from 'src/db/entity/roles.entity';
@@ -12,6 +12,8 @@ import { teamQueries } from 'src/repositoriers/team-table';
 import { userQueries } from 'src/repositoriers/user-table';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { requireAdmin } from 'src/middleware/requireAdmin';
+import { restrictUser } from 'src/middleware/restrictUser';
 
 @Module({
   imports: [
@@ -22,6 +24,30 @@ import { UserService } from './user.service';
     })
   ],
   controllers: [UserController],
-  providers: [UserService, JwtStrategy,userQueries,requestsQueries,teamQueries]
+  providers: [UserService, JwtStrategy, userQueries,requestsQueries,teamQueries]
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(requireAdmin)
+      .exclude(
+        {path:'/user/requests', method:RequestMethod.GET},
+        {path:'/user/profile', method:RequestMethod.GET}
+      )
+      .forRoutes(
+        { path: '/manager/:id', method: RequestMethod.GET },
+        { path: '/user/:id/ban', method: RequestMethod.POST },
+        { path: '/managers', method: RequestMethod.GET }
+    )
+      .apply(restrictUser)
+      .exclude(
+        { path: '/user/requests', method: RequestMethod.GET },
+        {path:'/user/profile', method:RequestMethod.GET}
+      )
+      .forRoutes(
+          {path: '/requests/:id', method: RequestMethod.PATCH},
+          {path: '/user/:id', method: RequestMethod.GET},
+          {path: '/requests', method: RequestMethod.GET},
+      )
+  }
+}
