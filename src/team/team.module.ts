741f8422d4891cdb987fbd2d849579bd6cebc971
Shpackk <victorija45@gmail.com>
2021-09-21 00:43:33 +0300

    middleware to check role added

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtStrategy } from 'src/passport/jwt.strategy';
+import { JwtStrategy } from 'src/passport/jwtauth/jwt.strategy';
 import { Requests } from 'src/db/entity/requests.entity';
 import { Team } from 'src/db/entity/teams.entity';
 import { User } from 'src/db/entity/user.entity';
@@ -9,11 +9,17 @@ import { teamQueries } from 'src/repositoriers/team-table';
 import { userQueries } from 'src/repositoriers/user-table';
 import { TeamController } from './team.controller';
 import { TeamService } from './team.service';
-import { AdminStrategy } from 'src/passport/jwt.admin.strategy';
+import { requireAdmin } from 'src/middleware/requireAdmin';
 
 @Module({
   imports:[TypeOrmModule.forFeature([Requests, User,Team])],
   controllers: [TeamController],
-  providers: [TeamService, JwtStrategy,AdminStrategy, userQueries,requestsQueries,teamQueries]
+  providers: [TeamService, JwtStrategy, userQueries,requestsQueries,teamQueries]
 })
-export class TeamModule {}
+export class TeamModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(requireAdmin)
+      .forRoutes({path: 'team/:id/kick', method:RequestMethod.DELETE})
+  }
+}
