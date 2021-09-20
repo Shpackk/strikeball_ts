bf0034010d9c0f13450e07d5bff583a96ec519e6
Shpackk <victorija45@gmail.com>
2021-09-21 00:39:57 +0300

    fields validation

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -1,6 +1,22 @@
+import {  IsEmail, IsString, Length, Matches } from "class-validator"
+
 export class RegUserDto {
+    @IsEmail({},{
+        message:"Bad Email"
+    })
     readonly email: string
+
+    @Length(4, 20, {
+        message:"Password Lenght Should Be At Least 4 c.long till 20"
+    })
     readonly name: string
+
+    @IsString()
     role: string
+
+    @IsString()
+    @Length(6, 20)
     password: string
+    
+    picture?: any
 }
