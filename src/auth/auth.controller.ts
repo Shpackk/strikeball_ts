9eed17464f222c87818cf7f0146015224821d102
Shpackk <victorija45@gmail.com>
2021-09-21 19:39:26 +0300

    google auth route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Post, Request, UseGuards, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseInterceptors, UploadedFile, UseGuards, Req } from '@nestjs/common';
 import { diskStorage } from 'multer';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
 import { RegUserDto } from './dto/register-user.dto';
 import { fileNameGen, imageFileFilter } from 'src/services/fileUploadHandler';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -25,6 +26,16 @@ export class AuthController {
     @Post('/login')
     async login(@Body() user: LoginUserDto): Promise<Object> {
         return await this.authService.login(user)
+    }
+
+    @UseGuards(AuthGuard('google'))
+    @Get('/google')
+    authGoogle(@Req() req) {}
+
+    @UseGuards(AuthGuard('google'))
+    @Get('/google/callback')
+    googleCallBack(@Req() req) {
+        return this.authService.googleLogin(req.user)
     }
 
 }
