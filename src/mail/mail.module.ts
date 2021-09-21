8258680fa47f7e571fa7a513b3e04e9504e14767
Shpackk <victorija45@gmail.com>
2021-09-21 18:30:27 +0300

    added env variables to connect

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,17 +1,19 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MailService } from './mail.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({envFilePath:'.env'}),
     MailerModule.forRoot({
       transport: {
-        host: 'smtp.gmail.com',
-        port:587,
-        secure: false,
+        host: process.env.SMTP_HOST_MAILER,
+        port:Number(process.env.MAILER_PORT),
+        secure: true,
         auth: {
-          user: 'victorijja45@gmail.com',
-          pass: 'Gfifhtctyxer11',
+          user: process.env.MAILER_USER,
+          pass: process.env.MAILER_USER_PASSWORD,
         },
       },
       defaults: {
