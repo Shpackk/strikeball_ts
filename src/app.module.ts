9f8fed9119ecdc36675097fb1cc6a2dfcdebcf1a
Shpackk <victorija45@gmail.com>
2021-09-21 04:09:48 +0300

    env support added

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,13 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TeamModule } from './team/team.module';
 import { MailModule } from './mail/mail.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // no need to import into other modules
+      envFilePath:'.env'
+    }),
     TypeOrmModule.forRoot(),
     UserModule,
     AuthModule,
