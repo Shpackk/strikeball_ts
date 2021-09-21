4c26053d3c776be8f4f8e03c48801aa8d7958d3e
Shpackk <victorija45@gmail.com>
2021-09-21 18:31:08 +0300

    env variable for port

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,15 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'static'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3000, () => {
-    console.log('OMG WE ARE LIVE ON 3000')
+  await app.listen(PORT, () => {
+    console.log(`OMG WE ARE LIVE ON ${PORT}`)
   });
 }
 bootstrap();
