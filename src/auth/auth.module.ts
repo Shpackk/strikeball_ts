bcfc18df1c65b667acebd54335d06417be7c13e9
Shpackk <victorija45@gmail.com>
2021-09-21 23:14:09 +0300

    added mongologs middleware

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Banlist } from 'src/db/entity/banlist.entity';
@@ -12,10 +12,14 @@ import { AuthService } from './auth.service';
 import { JwtStrategy } from '../passport/jwtauth/jwt.strategy';
 import { SocketGateWay } from 'src/services/socket.gateway';
 import { GoogleStrategy } from 'src/passport/jwtauth/google.strategy';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Log, LogSchema } from 'src/db/mongo/log.schema';
+import { MongoLoger } from 'src/middleware/mongoLoger';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, Roles, Banlist, Requests]),
+    MongooseModule.forFeature([{name: Log.name,schema:LogSchema}]),
     JwtModule.register({
       secret: 'supersecret',
       signOptions:{expiresIn: '24h'}
@@ -24,4 +28,10 @@ import { GoogleStrategy } from 'src/passport/jwtauth/google.strategy';
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy,GoogleStrategy,userQueries,requestsQueries, SocketGateWay]
 })
-export class AuthModule {}
+export class AuthModule implements NestModule  {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(MongoLoger)
+      .forRoutes(AuthController)
+  }
+}
