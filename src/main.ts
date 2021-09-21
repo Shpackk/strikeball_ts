869dfd3f5e9f7d80039c24e9099b15ac89e797eb
Shpackk <victorija45@gmail.com>
2021-09-21 04:09:31 +0300

    whitespace deleted

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './middleware/excepionHanlder';
+import { join } from 'path'
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'static'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000, () => {
