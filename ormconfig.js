af0738da6310ec3f524236e293eb63586d917f87
Shpackk <victorija45@gmail.com>
2021-09-23 02:54:21 +0300

    dir path fix for docker

---
+++ ormconfig.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+   "type": process.env.DB_TYPE,
+   "host": process.env.DB_HOST,
+   "port": process.env.DB_PORT,
+   "username": process.env.DB_USERNAME,
+   "password": process.env.DB_PASSWORD,
+   "database": process.env.DB_NAME,
+   "synchronize": false,
+   "logging": false,
+   "entities": [
+      __dirname + '/../**/**.entity{.ts,.js}'
+   ],
+   "migrations": [
+      "dist/db/migrations/*.{ts,js}"
+   ],
+   "subscribers": [
+      "dist/db/subscriber/**/*.{ts.js}"
+   ],
+   "cli": {
+      "entitiesDir": "src/db/entity",
+      "migrationsDir": "src/db/migrations",
+      "subscribersDir": "src/subscriber"
+   }
+}
+
