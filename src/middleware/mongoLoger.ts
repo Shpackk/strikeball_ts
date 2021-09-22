9c35b558c04d39c9fc2364ab26d5030ca428a86d
Shpackk <victorija45@gmail.com>
2021-09-23 02:59:01 +0300

    path fix for docker, refactored user extraction method

--- src/middleware/mongoLoger.ts
+++ src/middleware/mongoLoger.ts
@@ -2,7 +2,8 @@ import { NestMiddleware } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { NextFunction, Request, Response } from "express";
 import { Model } from "mongoose";
-import { Log, LogDocument } from "src/db/mongo/log.schema";
+import * as jwt from "jsonwebtoken"
+import { Log, LogDocument } from "../../src/db/mongo/log.schema";
 
 export class MongoLoger implements NestMiddleware {
     constructor(
@@ -12,13 +13,15 @@ export class MongoLoger implements NestMiddleware {
     
     async use(req: Request,res: Response, next: NextFunction): Promise<void> {
         try {
+            let user;
+            const token = req.header('authorization') ? req.header('authorization').split(' ')[1] : null
+            user = token ? jwt.decode(token) : null
             const log = {
-                user: req.user ? req.user : (req.body ? req.body.email : null),
+                user: user ? user.email : 'Unathorized',
                 method: req.method,
                 url: req.originalUrl,
                 requestBody: JSON.stringify(req.body)
             }
-            console.log(log)
             await new this.model(log).save()
             next()
         } catch (error) {
