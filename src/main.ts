0f4719b3c56014bc283777969ac166a57fbfe41b
Shpackk <victorija45@gmail.com>
2021-09-27 21:09:38 +0300

    path join changed to path resolve due to docker conflit

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './middleware/excepionHanlder';
-import { join } from 'path'
+import { resolve } from 'path'
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'static'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.useStaticAssets(resolve('./src/static'));
+  app.setBaseViewsDir(resolve('./src/views'));
   app.setViewEngine('ejs');
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(new ValidationPipe())
